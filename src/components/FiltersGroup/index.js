e8638b5577ee406782a7433f2891ea7b7237617b
Surya T N <suryatej.nim@yahoo.com>
2022-10-22 16:30:34 +0000

    Implemented JSX elements in FiltersGroup component to render the intended UI

--- src/components/FiltersGroup/index.js
+++ src/components/FiltersGroup/index.js
@@ -1,6 +1,6 @@
 import './index.css'
 
-const FiltersGroup = () => {
+const FiltersGroup = props => {
   const {categoryData, ratingData, filtersChangeHandler} = props
 
   // Have to use inputSearchString, selectedCategoryId, selectedRatingId
@@ -23,6 +23,53 @@ const FiltersGroup = () => {
         placeholder="Search"
         onChange={onSearchInputChange}
       />
+
+      <div className="single-filter-container">
+        <p className="filter-header">Category</p>
+        <ul className="filter-options-container">
+          {categoryData.map(categoryDataItem => (
+            <li className="filter-option">
+              <button
+                type="button"
+                className="filter-option-button"
+                onClick={() =>
+                  filtersChangeHandler({
+                    selectedCategoryId: categoryDataItem.categoryId,
+                  })
+                }
+              >
+                {categoryDataItem.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="single-filter-container">
+        <p className="filter-header">Rating</p>
+        <ul className="filter-options-container">
+          {ratingData.map(ratingDataItem => (
+            <li className="filter-option">
+              <button
+                type="button"
+                className="filter-option-button"
+                onClick={() =>
+                  filtersChangeHandler({
+                    selectedRatingId: ratingDataItem.ratingId,
+                  })
+                }
+              >
+                <img
+                  className="filter-option-button-img"
+                  src={ratingDataItem.imageUrl}
+                  alt={`rating ${ratingDataItem.ratingId}`}
+                />{' '}
+                {} & up
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
