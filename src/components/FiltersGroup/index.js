2634dd001bd3857d2655a1dbf255c5eea657c004
Surya T N <suryatej.nim@yahoo.com>
2022-10-22 14:41:40 +0000

    Added code in FiltersGroup to render the search input JSX element with appropriate change handler to be called for lifting state up to parent component AllProductsSection

--- src/components/FiltersGroup/index.js
+++ src/components/FiltersGroup/index.js
@@ -1,10 +1,30 @@
 import './index.css'
 
-const FiltersGroup = () => (
-  <div className="filters-group-container">
-    <h1>Filters Group</h1>
-    {/* Replace this element with your code */}
-  </div>
-)
+const FiltersGroup = () => {
+  const {categoryData, ratingData, filtersChangeHandler} = props
+
+  // Have to use inputSearchString, selectedCategoryId, selectedRatingId
+  // as object keys with appropriate values, when calling filtersChangeHandler
+  // method with changed filter data object as input.
+  const onSearchInputChange = searchInputChangeEvent => {
+    const updatedSearchString = searchInputChangeEvent.target.value
+    const changedSearchInputFilterObject = {
+      inputSearchString: updatedSearchString,
+    }
+
+    filtersChangeHandler(changedSearchInputFilterObject)
+  }
+
+  return (
+    <div className="filters-group-container">
+      <input
+        type="search"
+        className="product-search-container"
+        placeholder="Search"
+        onChange={onSearchInputChange}
+      />
+    </div>
+  )
+}
 
 export default FiltersGroup
